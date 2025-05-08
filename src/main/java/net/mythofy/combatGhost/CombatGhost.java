ec3c5816d9e1510bc391e20c14c1c3391cae05b6
admin <jonah.novoseller@icloud.com>
2025-05-08 18:53:46 -0500

    Initial commit of CombatGhost plugin

---
+++ src/main/java/net/mythofy/combatGhost/CombatGhost.java
@@ -0,0 +1,17 @@
+package net.mythofy.combatGhost;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CombatGhost extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
